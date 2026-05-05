3c93b2fd548f48005dc045b2ed4b31edacdc14de
ROBERT JASON ALVA VARGAS <r.alva@pucp.edu.pe>
2026-05-05 20:00:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7611158483-8291f",
+  "appId": "1:500477603991:web:004516d340344f3c6cf9d2",
+  "apiKey": "AIzaSyC9jO3Hv7UVPXxJmnXSqHb017PmIpgJLVY",
+  "authDomain": "studio-7611158483-8291f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "500477603991"
+};
